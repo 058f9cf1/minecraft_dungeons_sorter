b666e9b0c6a6d4dec89687dddfb09c7ce72fb4fd
058f9cf1 <058f9cf1@tutanota.com>
2026-04-16 00:42:04 +0100

    Added comments and rearranged code

--- src/minecraft_dungeons_sorter.py
+++ src/minecraft_dungeons_sorter.py
@@ -10,12 +10,16 @@ key = b"\x5c\xeb\x9d\x0a\xeb\xb9\x5a\xc0\x27\x0b\x0a\xf6\x75\x3d\xfc\x0e\xe3\xe6
 
 
 def finish(message):
+	"""Display a message to the user then exit the program"""
+
 	print(message)
 	input("Press Enter to quit.")
 	raise SystemExit
 
 
 def is_valid_save(file):
+	"""Determine if a file is a valid Minecraft Dungeons save"""
+
 	with open(file, 'rb') as f:
 		header = f.read(8)
 
@@ -23,13 +27,17 @@ def is_valid_save(file):
 
 
 def find_save():
+	"""Search the disk for Minecraft Dungeons saves"""
+
 	print("TODO: Find Save.")
 
 
 def decrypt_save(file):
+	"""Decrypt a Minecraft Dungeons save"""
+
 	with open(file, 'rb') as f:
 		text = f.read()
-	
+
 	cipher = AES.new(key, AES.MODE_ECB)
 	dec = cipher.decrypt(text[8:])
 
@@ -37,6 +45,8 @@ def decrypt_save(file):
 
 
 def encrypt_data(data):
+	"""Encrypt a Minecraft Dungeons save"""
+
 	cipher = AES.new(key, AES.MODE_ECB)
 	padding = ' ' * (16 - (len(data) % 16))
 	enc = cipher.encrypt(bytearray(data + padding, 'utf-8'))
@@ -45,29 +55,36 @@ def encrypt_data(data):
 
 
 def write_file(file, data):
+	"""Write the data back to file"""
+
 	with open(file, 'wb') as f:
 		f.write(data)
 	print("Written to", file)
 
+	return True
+
 
 if __name__ == "__main__":
+	# Saves given via args
 	if len(argv) > 1:
 		written = False
 		for file in argv[1:]:
+
+			# Single save file
 			if is_valid_save(file):
 				data = decrypt_save(file)
 				data = sort_items(data)
 				data = encrypt_data(data)
-				#write_file(file.rsplit(".", 1)[0] + "_sorted.dat", data)
-				write_file(file, data)
-				written = True
+				written = write_file(file, data)
+
 			else:
 				print(file, "isn't a valid Minecraft Dungeons save.")
-		if written:
-			finish("Sucess!")
-		else:
+
+		if not written:
 			finish("Nothing written.")
 	else:
+		print("No saves provided, searching disk...")
 		find_save()
-		#TODO
-		finish("Sucess!")
+		# TODO
+
+	finish("Success!")
